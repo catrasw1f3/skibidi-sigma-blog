23eedaa7435ff59743dc9078b2e79bee02f9541d
catrasw1f3 <namiras.456@gmail.com>
2025-03-31 03:59:28 -0700

    like tweaked irt a bit no more arrows also fixed the hitbox for the time machine

--- assets/js/adventureGame/GameLevelStarWars.js
+++ assets/js/adventureGame/GameLevelStarWars.js
@@ -18,11 +18,6 @@ class GameLevelStarWars {
         src: image_src_bg,
         pixels: { height: 570, width: 1025 }
     };
-    const image_data_bg2 = {
-      id: 'Background2',
-      src: image_src_bg2,
-      pixels: { height: 570, width: 1025 }
-  };
 
     // Player data for Tourist
     const sprite_src_tourist = "/images/gamify/tourist.png"; // be sure to include the path
@@ -81,69 +76,22 @@ class GameLevelStarWars {
       src: sprite_src_timemachine,
       SCALE_FACTOR: 3,  // Adjust this based on your scaling needs
       ANIMATION_RATE: 100,
-      pixels: {width: 150, height: 194},
-      INIT_POSITION: { x: (width / 2) - 70, y: (height / 2) - 240 }, // Adjusted position
+      pixels: {width: 97, height: 143},
+      INIT_POSITION: { x: (width / 2) - 75, y: (height / 2) - 240 }, // Adjusted position
       orientation: {rows: 1, columns: 1 },
       down: {row: 0, start: 0, columns: 1 },  // This is the stationary npc, down is default 
-      hitbox: { widthPercentage: 0.1, heightPercentage: 0.2 },
+      hitbox: { widthPercentage: 0.05, heightPercentage: 0.05 },
     };
 
-    // Arrow data, temporary sprite for testing
-    const sprite_src_arrow = "/images/gamify/arrow.png"; // be sure to include the path
-    const sprite_data_arrow1 = {
-        id: 'Arrow1',
-        greeting: "Simulate explosive action!",
-        // define image/sprite data
-        src: sprite_src_arrow,
-        pixels: {height: 500, width: 500}, // height and width of the image
-        orientation: {rows: 1, columns: 1 }, // normalized rows and columns in the sprite
-        // define size, position, adjustments for hitbox
-        SCALE_FACTOR: 30,  // Start small
-        INIT_POSITION_RATIO: { x: 1 / 1.78, y: 1 / 3.3 }, // Ratios for initial position
-        hitbox: { widthPercentage: 0.1, heightPercentage: 0.2 },
-        // define animation properties
-        TRANSLATE_SCALE_FACTOR: 10, // Grow to this size at end translation
-        TRANSLATE_POSITION_RATIO: { x: 1 / 2.78, y: 1 / 2.9 }, // Ratios for translate position
-        TRANSLATE_SIMULATION: {miliseconds: 500 }, // 1 second
-        down: {row: 0, start: 0, columns: 1, spin: 4},  // down is default
-     };
-
-     // Arrow data, temporary sprite for testing
-    const sprite_data_arrow2 = {
-        id: 'Arrow',
-        greeting: "Simulate explosive action!",
-        // define image/sprite data
-        src: sprite_src_arrow,
-        pixels: {height: 500, width: 500}, // height and width of the image
-        orientation: {rows: 1, columns: 1 }, // normalized rows and columns in the sprite
-        // define size, position, adjustments for hitbox
-        SCALE_FACTOR: 60,  // Start small 
-        INIT_POSITION_RATIO: { x: 1 / 8, y: 1 / 1.95 }, // Ratios for initial position
-        hitbox: { widthPercentage: 0.1, heightPercentage: 0.2 },
-        // define animation properties
-        TRANSLATE_SCALE_FACTOR: 20, // Grow to this size at end translation
-        TRANSLATE_POSITION_RATIO: { x: 1 / 20, y: 1 / 1.9 }, // Ratios for translate position
-        TRANSLATE_SIMULATION: {miliseconds: 500 }, // 1 second
-        down: {row: 0, start: 0, columns: 1, spin: 4},  // down is default
-     };
+    
 
     // List of objects definitions for this level
     this.classes = [
       { class: Background, data: image_data_bg },
-      { class: Background, data: image_data_bg2 },
       { class: Player, data: sprite_data_tourist },
       { class: Npc, data: sprite_data_tombguard },
       { class: Npc, data: sprite_data_timemachine },
-      { class: Projectile, data: sprite_data_arrow1 },
-      { class: Projectile, data: sprite_data_arrow2 },
     ];
-
-    // Add event listener for the 'E' key to change the background
-    window.addEventListener('keydown', (event) => {
-      if (event.key === 'E' || event.key === 'e') {
-        this.changeBackground(image_data_bg, image_data_bg2);
-      }
-    });
   }
 
   /**
